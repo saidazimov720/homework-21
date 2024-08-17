923752e66d6d55a3b3b3e36e9dc752bb2d44b740
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-17 20:01:05 +0500

    SuperAdmin

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const adminPasword = "admin123";
+class SuperAdmin {
+    constructor(name, age, edit, remove) {
+        this.name = name;
+        this.age = age;
+        this.edit = edit;
+        this.remove = remove;
+        this.message = "";
+    }
+}
